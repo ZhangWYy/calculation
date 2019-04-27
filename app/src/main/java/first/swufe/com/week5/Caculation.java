2d74e7b21e8d467ea83716a05f623412390cbd84
ZhangWYy <1109827726@qq.com>
2019-04-27 22:57:31 +0800

    maintain message

--- app/src/main/java/first/swufe/com/week5/Caculation.java
+++ app/src/main/java/first/swufe/com/week5/Caculation.java
@@ -1,7 +1,9 @@
 package first.swufe.com.week5;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +25,7 @@ public class Caculation extends AppCompatActivity implements View.OnClickListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caculation);
+        Log.i(TAG, "onCreate: ");
 
         scoreA = findViewById(R.id.scoreA);
         scoreB = findViewById(R.id.scoreB);
@@ -99,5 +102,46 @@ public class Caculation extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
 
+    }
+
+    private final String TAG = "calculation";
+
+    protected void onStart(){
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+    
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String scorea=((TextView)findViewById(R.id.scoreA)).getText().toString();
+        String scoreb=((TextView)findViewById(R.id.scoreB)).getText().toString();
+        Log.i(TAG, "onSaveInstanceState: ");
+        outState.putString("teama_score",scorea);
+        outState.putString("teamb_score",scoreb);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String scorea = savedInstanceState.getString("teama_score");
+        String scoreb = savedInstanceState.getString("teamb_score");
+        Log.i(TAG, "onRestoreInstanceState: ");
+        ((TextView)findViewById(R.id.scoreA)).setText(scorea);
+        ((TextView)findViewById(R.id.scoreB)).setText(scoreb);
     }
 }
